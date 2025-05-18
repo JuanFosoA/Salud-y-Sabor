a97adc7cef665039ae3cb33682c89757b98c4910
YioJoe <yioyiomain@gmail.com>
2025-05-17 21:23:07 -0500

    Adding detailcard

--- frontend/app-salud-sabor/app/(tabs)/index.tsx
+++ frontend/app-salud-sabor/app/(tabs)/index.tsx
@@ -2,12 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import SimpleCard from '@/components/atoms/SimpleCard'
 import IndexModule from '@/models/tabs/IndexModule'
+import DetailedCard from '@/components/molecules/DetailedCard'
 
 const index = () => {
   return (
     <View>
       <Text>Bienvenido</Text>
       <SimpleCard title='Cereal' imageName='https://media.istockphoto.com/id/1290306471/es/foto/anillos-de-ma%C3%ADz-coloridos-en-cuenco-aislados-sobre-fondo-blanco.jpg?s=612x612&w=0&k=20&c=gI5clA4HTZ76Lz-3-6QZahVxFdPM2CTNG6bkSvoYqNM='/>
+      <DetailedCard title='cereal' author='Juan' imageName='https://media.istockphoto.com/id/1290306471/es/foto/anillos-de-ma%C3%ADz-coloridos-en-cuenco-aislados-sobre-fondo-blanco.jpg?s=612x612&w=0&k=20&c=gI5clA4HTZ76Lz-3-6QZahVxFdPM2CTNG6bkSvoYqNM=' />
       <IndexModule/>
     </View>
   )
