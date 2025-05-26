b29eecd51a1475ff17577106ae5c3bd30f4a2bf7
YioJoe <yioyiomain@gmail.com>
2025-05-26 14:49:06 -0500

    Fix: Adding cors

--- backend/salud-y-sabor/src/main.ts
+++ backend/salud-y-sabor/src/main.ts
@@ -5,6 +5,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',            
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
